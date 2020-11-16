193af021a2c2b405dc56b2d4a2abdb0bfa5096fd
navinm12 <navinmurugaiyan12@gmail.com>
2020-11-16 12:27:55 +0530

    V1.0.2

--- Ks-cdn.js
+++ Ks-cdn.js
@@ -1,23 +1,20 @@
 console.log('ready!')
-
 var start = new Date()
-
 window.addEventListener('click', function (e) {
   var firstClick = sessionStorage.getItem('firstClick')
-  // console.log("firstClick");
   if (firstClick == 'true') {
     var end = new Date()
     var userIpv4
     var urlpage = window.location.href
-    $.getJSON('https://api.ipify.org?format=json', function (data) {
-      // Setting text of element P with id gfg
-      userIpv4 = data.ip
-      sessionStorage.setItem('ip', userIpv4)
-    })
-    //send data to the server before page close
-    var ip = sessionStorage.getItem('ip')
-    // console.log(ip);
 
+    fetch('https://api.ipify.org?format=json')
+      .then((response) => response.json())
+      .then((response) => {
+        userIpv4 = response.ip
+        sessionStorage.setItem('ip', userIpv4)
+      })
+      .catch((err) => console.log(err))
+    var ip = sessionStorage.getItem('ip')
     fetch('https://knews-vscaleup-api.herokuapp.com/api/analytics/create', {
       headers: {
         Accept: 'application/json',
@@ -40,7 +37,5 @@ window.addEventListener('click', function (e) {
 })
 
 window.onload = function () {
-  // console.log("onloadworks");
   sessionStorage.setItem('firstClick', 'true')
-  // sessionStorage.setItem("start",start.toString());
 }
