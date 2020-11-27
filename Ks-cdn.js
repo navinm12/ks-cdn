6d51e38e6a22f20b076d47149d8b7987f9982e55
navinm12 <navinmurugaiyan12@gmail.com>
2020-11-27 10:30:38 +0530

    OnloadDataFetch

--- Ks-cdn.js
+++ Ks-cdn.js
@@ -5,13 +5,10 @@ async function fetchIp() {
     let pageUrl = window.location.href
     let currentIp = jsonIp.ip
 
-    // document.addEventListener('click', function () {
     if (
       sessionStorage.getItem('user_ip') !== currentIp ||
       sessionStorage.getItem('page_url') !== pageUrl
     ) {
-      // let end = TimeMe.getTimeOnCurrentPageInSeconds()
-
       fetch('https://knews-vscaleup-api.herokuapp.com/api/analytics/create', {
         headers: {
           Accept: 'application/json',
@@ -19,23 +16,17 @@ async function fetchIp() {
         },
         method: 'post',
         body: JSON.stringify({
-          // timeSpent: end,
           ip: currentIp,
           page: pageUrl,
           appVersion: window.navigator.appVersion,
-          pageTitle: document.title.split('|')[0],
-          // category: document.title.split('|')[1],
         }),
       })
         .then((response) => {
-          sessionStorage.setItem('page_title', document.title)
           sessionStorage.setItem('user_ip', currentIp)
           sessionStorage.setItem('page_url', pageUrl)
         })
         .catch((err) => console.log(err))
     }
-    // })
   }
 }
-
 fetchIp()
